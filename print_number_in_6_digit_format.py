21da3fe2ad3d20f7735bd305efa4f088e53a152a
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 18:45:57 +0800

    cleared the code line 3 to 7

--- print_number_in_6_digit_format.py
+++ print_number_in_6_digit_format.py
@@ -1,11 +1,5 @@
 #Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
-#FUNCTION main():
-# PRINT "Please enter a number between 0 and 1000:"
-# number = get user input (as an integer)
-# six_digit_number = Format number AS A 6-DIGIT STRING WITH LEADING ZEROS
-# PRINT "Your number in 6-digit:", six_digit_number
-
 def main():
     number = int(input("Please enter a number between 0 and 1000: "))
     
