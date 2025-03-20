51fb84498b6f89f331ee779d46b858c29a7a6e0d
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 18:01:15 +0800

    added user input and instructions, used .lstrip to remove spaces from the user input, lastly printed the cleaned_name

--- print_input_with_no_space.py
+++ print_input_with_no_space.py
@@ -1,8 +1,10 @@
 #Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
-#FUNCTION main():
-#PRINT "enter smth"
-#full_name = GET user input
-#cleaned_name = REMOVE LEADING SPACES FROM full_name
-#PRINT "Your name without leading spaces is:", cleaned_name
-#how
+def main():
+    full_name = input("Please enter your full name with leading spaces: ")
+    
+    cleaned_name = full_name.lstrip()
+    print("Your name without leading spaces is:", cleaned_name)
+
+if __name__ == "__main__":
+    main()
