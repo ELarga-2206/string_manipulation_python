c3026aaebf2268de3103909dbc0dc153fcc17096
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 15:18:52 +0800

    added user input condition, used to_snake_case function that converts the input string to snake_case (lowercase letters with underscores replacing spaces, then will print the output

--- print_input_in_snake_case.py
+++ print_input_in_snake_case.py
@@ -1,10 +1,14 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 
-#get user input
-#replace to snake case, use .replace
-#must replace spaces with underscore
-#replace capitals with lower cases
-
 def to_snake_case(name):
     snake_case_name = name.lower().replace(" ", "_")
     return snake_case_name
+
+def main():
+    full_name = input("Please enter your full name in incorrect casing: ")
+    
+    snake_case_name = to_snake_case(full_name)
+    print("Your name in snake_case is:", snake_case_name)
+
+if __name__ == "__main__":
+    main()
