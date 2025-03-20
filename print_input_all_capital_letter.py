59d2f1684a109dd9403bda86223606cf99485dc9
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 11:46:40 +0800

    initial pseudo code for prog3

--- print_input_all_capital_letter.py
+++ print_input_all_capital_letter.py
@@ -1,1 +1,7 @@
 #Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+#UNCTION main():
+# PRINT "Please enter your full name:"
+# full_name = GET user input
+# name = CONVERT full_name TO ALL CAPITAL LETTERS
+# PRINT "Your name in all capital letters is:", capiname
