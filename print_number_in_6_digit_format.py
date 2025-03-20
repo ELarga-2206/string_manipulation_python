f3e851d72e308bccd6b76104f3fcadf920ca77c0
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 18:36:53 +0800

    initial code idea, used ':06'format specifier to add 0

--- print_number_in_6_digit_format.py
+++ print_number_in_6_digit_format.py
@@ -5,3 +5,13 @@
 # number = get user input (as an integer)
 # six_digit_number = Format number AS A 6-DIGIT STRING WITH LEADING ZEROS
 # PRINT "Your number in 6-digit:", six_digit_number
+
+def main():
+    number = int(input("Please enter a number between 0 and 1000: "))
+    
+    six_digit_number = "{number:06}"
+    
+    print("6-digit format is:", six_digit_number)
+
+if __name__ == "__main__":
+    main()
