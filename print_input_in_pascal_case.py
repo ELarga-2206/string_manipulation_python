3bbdb9284242805983433115a623b14ce02a3d0e
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 13:29:40 +0800

    added initial pseudocode for proper code requirements and guidelines

--- print_input_in_pascal_case.py
+++ print_input_in_pascal_case.py
@@ -1,1 +1,11 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+
+#FUNCTION to_pascal_case(name):
+# words = SPLIT name INTO INDIVIDUAL WORDS
+# pascal_case_name = EMPTY STRING
+# FOR each word IN words:
+# capitalized_word = CAPITALIZE first letter of word
+# pascal_case_name = pascal_case_name + capitalized_word
+
+#pascal_case_name = CALL to_pascal_case(full_name)
+# PRINT "Your name in PascalCase is:", pascal_case_name
