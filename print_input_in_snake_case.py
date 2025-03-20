06eb5afd2f5f6d6f5fb06396c999488f59d0ba9d
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 14:50:21 +0800

    added pseudo code, added a function that uses .lower and .replace to replace spaces with underscores from the user's input

--- print_input_in_snake_case.py
+++ print_input_in_snake_case.py
@@ -1,1 +1,10 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+
+#get user input
+#replace to snake case, use .replace
+#must replace spaces with underscore
+#replace capitals with lower cases
+
+def to_snake_case(name):
+    snake_case_name = name.lower().replace(" ", "_")
+    return snake_case_name
