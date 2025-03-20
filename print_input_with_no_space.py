b227e294e8defe31a6f43efdfc3ce185d1db19f4
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 15:30:19 +0800

    added initial pseudocode for guide

--- print_input_with_no_space.py
+++ print_input_with_no_space.py
@@ -1,1 +1,8 @@
 #Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+#FUNCTION main():
+#PRINT "enter smth"
+#full_name = GET user input
+#cleaned_name = REMOVE LEADING SPACES FROM full_name
+#PRINT "Your name without leading spaces is:", cleaned_name
+#how
