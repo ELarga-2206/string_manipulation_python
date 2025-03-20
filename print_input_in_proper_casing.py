1abe2e5f9b7d8c58ba83fbd17b9f9953ae3538b1
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 14:13:55 +0800

    added initial pseudocode

--- print_input_in_proper_casing.py
+++ print_input_in_proper_casing.py
@@ -1,1 +1,13 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+#how to change into proper casing?
+
+#FUNCTION to_proper_case(name):
+# proper_case_name = CONVERT name TO PROPER CASING (first letter of each word capitalized, rest lowercase)
+# RETURN proper_case_name
+# FUNCTION main():
+# PRINT "Please enter your full name in incorrect casing:"
+# full_name = GET user input
+#print proper_case_name
+
+
