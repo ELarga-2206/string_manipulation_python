9db796a166c5873d691e43b3323889f919c737d0
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 11:55:22 +0800

    completed the code using '.upper' function, cleaned and tested

--- print_input_all_capital_letter.py
+++ print_input_all_capital_letter.py
@@ -1,7 +1,11 @@
 #Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
-#UNCTION main():
-# PRINT "Please enter your full name:"
-# full_name = GET user input
-# name = CONVERT full_name TO ALL CAPITAL LETTERS
-# PRINT "Your name in all capital letters is:", capiname
+def main():
+    full_name = input("enter full name: ")
+    
+    capitalized_name = full_name.upper()
+    
+    print("name in all capital letters:", capitalized_name)
+
+if __name__ == "__main__":
+    main()
