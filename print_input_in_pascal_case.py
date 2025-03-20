02171b47dd151323b17bf2fef605eb376b3e1945
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 14:09:22 +0800

    added user input condition, used .split to convert user input to list, reconfigured and tested the code

--- print_input_in_pascal_case.py
+++ print_input_in_pascal_case.py
@@ -1,18 +1,18 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 
-#FUNCTION to_pascal_case(name):
-# words = SPLIT name INTO INDIVIDUAL WORDS
-# pascal_case_name = EMPTY STRING
-# FOR each word IN words:
-# capitalized_word = CAPITALIZE first letter of word
-# pascal_case_name = pascal_case_name + capitalized_word
-
-#pascal_case_name = CALL to_pascal_case(full_name)
-# PRINT "Your name in PascalCase is:", pascal_case_name
-
 def to_pascal_case(name):
     words = name.split()
     
     pascal_case_name = ''.join(word.capitalize() for word in words)
     
     return pascal_case_name
+
+def main():
+    full_name = input("enter your full name in random casing: ")
+    
+    pascal_case_name = to_pascal_case(full_name)
+    
+    print("PascalCase:", pascal_case_name)
+
+if __name__ == "__main__":
+    main()
