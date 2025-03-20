b9e0d3fa6183330efd26982ee9e4cb423c52d673
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 11:39:31 +0800

    removed line 3 to 16 (cleaned the source code)

--- print_each_character_in_reverse_casing.py
+++ print_each_character_in_reverse_casing.py
@@ -1,20 +1,5 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 
-#FUNCTION reverse_case(char):
-#IF char is lowercase:
-#RETURN uppercase version of char
-#ELSE IF char is uppercase:
-#RETURN lowercase version of char
-#ELSE: RETURN char (leave unchanged)
-
-#FOR each character in full_name:
-        #reversed_char = CALL reverse_case(character)
-        #reversed_case_name = reversed_case_name + reversed_char
-
-    #PRINT "Your name with reversed casing is lmao:", reversed_case_name smth
-
-#CALL main()
-
 def reverse_case(char):
     if char.islower():
         return char.upper()
