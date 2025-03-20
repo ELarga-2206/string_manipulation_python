dea476e511197444d1571ba4ba5337231ce0ca63
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 18:17:20 +0800

    added a pseudocode for guide

--- print_number_in_6_digit_format.py
+++ print_number_in_6_digit_format.py
@@ -1,1 +1,7 @@
 #Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
+#FUNCTION main():
+# PRINT "Please enter a number between 0 and 1000:"
+# number = get user input (as an integer)
+# six_digit_number = Format number AS A 6-DIGIT STRING WITH LEADING ZEROS
+# PRINT "Your number in 6-digit:", six_digit_number
