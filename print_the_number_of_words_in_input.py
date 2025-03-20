f8dceb757828a596d5a48feea228d3241f601143
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 19:13:11 +0800

    created a pseudocode

--- print_the_number_of_words_in_input.py
+++ print_the_number_of_words_in_input.py
@@ -1,1 +1,9 @@
 #Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+#FUNCTION main():
+# PRINT "Please enter a complete statement:"
+# statement = GET user input
+# words = SPLIT statement INTO A LIST OF WORDS
+# num_words = COUNT THE NUMBER OF WORDS IN THE LIST
+# PRINT "The number of words in your statement is:", num_words
+
