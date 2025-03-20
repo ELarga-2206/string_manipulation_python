a4a18f2d0320d4a3de3dac49fef1b803b6c00c28
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 11:37:19 +0800

    used .join to join the 2 inputs, goes through each charac and reverses its case, completed the overall main function

--- print_each_character_in_reverse_casing.py
+++ print_each_character_in_reverse_casing.py
@@ -23,7 +23,12 @@ def reverse_case(char):
     else:
         return char
     
-    def main():
-    # Ask user to input full name
-    #try reverse the casing (dunnow how)
-    #print result
+def main():
+    full_name = input("Please enter a name in incorrect casing: ")
+    
+    reversed_case_name = ''.join([reverse_case(char) for char in full_name])
+    
+    print("reversed case:", reversed_case_name)
+
+if __name__ == "__main__":
+    main()
