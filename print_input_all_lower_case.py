1b11aabd6c69243767c3f3df732810a9a073283b
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 12:00:34 +0800

    reused the code from a similar problem, replaced '.upper' with '.lower' to convert uppercase to lower

--- print_input_all_lower_case.py
+++ print_input_all_lower_case.py
@@ -1,1 +1,11 @@
 #Create a program that ask the user to input their fullname. Print the input in all lower case.
+
+def main():
+    full_name = input("enter your full name: ")
+    
+    lowercase_name = full_name.lower()
+    
+    print("name in all lowercase letters:", lowercase_name)
+
+if __name__ == "__main__":
+    main()
