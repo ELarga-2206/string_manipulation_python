fc388e3486d694577edeeecb165cd8992a30ed54
Elarga-2206 <earl.larga22@gmail.com>
2025-03-20 14:38:27 +0800

    added a line that convert user input to proper casing to store the result in the variable, 2nd line that prints the cased output, and last line that calls to the main function

--- print_input_in_proper_casing.py
+++ print_input_in_proper_casing.py
@@ -1,18 +1,13 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
 
-#how to change into proper casing?
-
-#FUNCTION to_proper_case(name):
-# proper_case_name = CONVERT name TO PROPER CASING (first letter of each word capitalized, rest lowercase)
-# RETURN proper_case_name
-# FUNCTION main():
-# PRINT "Please enter your full name in incorrect casing:"
-# full_name = GET user input
-#print proper_case_name
-
 def to_proper_case(name):
     proper_case_name = name.title()
     return proper_case_name
 
 def main():
     full_name = input("Please enter your full name in incorrect casing: ")
+    proper_case_name = to_proper_case(full_name)
+    print("Your name in proper casing is:", proper_case_name)
+
+if __name__ == "__main__":
+    main()
